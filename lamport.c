55df089b6f731d0976ea3e405c72a14c74e87331
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2015-11-07 15:04:00 -0500

    cache front and back

--- lamport.c
+++ lamport.c
@@ -11,6 +11,8 @@ struct LamportQueue
 {
     atomic_size_t front_;
     atomic_size_t back_;
+    size_t cached_front_;
+    size_t cached_back_;
     T data_[SIZE];
 };
 
@@ -18,13 +20,21 @@ void LamportQueue_init(struct LamportQueue *queue)
 {
        atomic_init(&queue->front_, 0);
        atomic_init(&queue->back_, 0);
+       queue->cached_front_ = queue->cached_back_ = 0;
 }
 
 bool LamportQueue_push(struct LamportQueue *queue, T elem)
 {
     size_t b, f;
     b = atomic_load_explicit(&queue->back_, memory_order_relaxed);
-    f = atomic_load_explicit(&queue->front_, memory_order_acquire);
+    f = queue->cached_front_;
+    if ((b + 1) % SIZE == f)
+    {
+	    queue->cached_front_ = f = atomic_load_explicit(&queue->front_, memory_order_acquire);
+    }
+    else
+    { /* front can only increase since the last time we read it, which means we can only get more space to push into.
+	 If we still have space left from the last time we read, we don't have to read again. */ }
     if ((b + 1) % SIZE == f)
     {
         return false;
@@ -40,7 +50,14 @@ bool LamportQueue_pop(struct LamportQueue *queue, T *elem)
 {
     size_t b, f;
     f = atomic_load_explicit(&queue->front_, memory_order_relaxed);
-    b = atomic_load_explicit(&queue->back_, memory_order_acquire);
+    b = queue->cached_back_;
+    if (b == f)
+    {
+	    queue->cached_back_ = b = atomic_load_explicit(&queue->back_, memory_order_acquire);
+    }
+    else
+    { /* back can only increase since the last time we read it, which means we can only get more items to pop from.
+	 If we still have items left from the last time we read, we don't have to read again. */ }
     if (b == f)
     {
         return false;
